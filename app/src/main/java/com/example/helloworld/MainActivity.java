33495b9bbd1723dcd96e4d580f4219e0655cac4a
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-07-30 01:57:27 +0100

    Tutorial - "Create your First Application (Part 2)"

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view) {
+        EditText editTxtName = findViewById(R.id.editTxtName);
+
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText(String.format("Hello %s", editTxtName.getText().toString()));
     }
 }
