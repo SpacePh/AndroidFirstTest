415b4f04a4f620a2dbb9ad925f2288b12748a535
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-08-02 02:21:52 +0100

    Tutorial - "User Interface Basics (Part 2)"

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -1,22 +1,61 @@
 package com.example.uibasics;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView txtHello;
+    private EditText edtTxtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello = findViewById(R.id.btnHello);
+        // First method
+//        btnHello.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println("Hello");
+//            }
+//        });
+        // Second method
+        btnHello.setOnClickListener(this);
+        btnHello.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(MainActivity.this, "Long Press", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        edtTxtName = findViewById(R.id.edtTxtName);
+        edtTxtName.setOnClickListener(this);
+        txtHello = findViewById(R.id.txtHello);
     }
 
-    public void onHelloBtnClicked(View view) {
-        TextView txtWelcome = findViewById(R.id.txtWelcome);
-        txtWelcome.setText("Hello Again");
-    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnHello:
+//                System.out.println("Hello");
+//                Toast.makeText(this, "Hello Button Clicked", Toast.LENGTH_SHORT).show();
+                txtHello.setText("Hello " + edtTxtName.getText().toString());
+                break;
+            case R.id.edtTxtName:
+                Toast.makeText(this, "Attempting to type something", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
 }
