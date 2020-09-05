1cdb40e293d3b51d72dcee6d27fd5495c5802dfd
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-09-05 23:30:22 +0100

    Tutorial - cleanup

--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -1,35 +1,14 @@
 package com.example.uibasics;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView txtHello;
-    private Button btnChangeFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtHello = findViewById(R.id.txtHello);
-        btnChangeFont = findViewById(R.id.btnChangeFont);
-
-        final Typeface typeface = ResourcesCompat.getFont(this, R.font.amsterdam);
-
-        btnChangeFont.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txtHello.setTypeface(typeface);
-            }
-        });
     }
-
 }
