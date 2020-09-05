711364f439d114901339c957e6cfbe12c2c07efe
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-09-05 15:45:21 +0100

    Tutorial - "RecyclerView (Part 1)"

---
+++ app/src/main/java/com/example/uibasics/ContactsRecViewAdapter.java
@@ -0,0 +1,50 @@
+package com.example.uibasics;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecViewAdapter.ViewHolder> {
+
+    private List<Contact> contacts = new ArrayList<>();
+
+    public ContactsRecViewAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txtName);
+        }
+    }
+}
--- app/src/main/java/com/example/uibasics/MainActivity.java
+++ app/src/main/java/com/example/uibasics/MainActivity.java
@@ -1,60 +1,20 @@
 package com.example.uibasics;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import com.google.android.material.card.MaterialCardView;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ConstraintLayout parent;
-    private Button btnShowSnackbar;
-    private MaterialCardView cardView;
+    private RecyclerView contactsRecView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        parent = findViewById(R.id.parent);
-        btnShowSnackbar = findViewById(R.id.button);
-        cardView = findViewById(R.id.cardView);
-        
-        cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Card Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnShowSnackbar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSnackbar();
-            }
-        });
-    }
-
-    private void showSnackbar() {
-        Snackbar.make(parent, "This is a Snackbar", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Retry", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(MainActivity.this, "Retry Clicked", Toast.LENGTH_SHORT).show();
-                    }
-                })
-//                .setActionTextColor(getResources().getColor(R.color.colorAccent))
-                .setActionTextColor(Color.RED)
-                .setTextColor(Color.YELLOW)
-                .show();
+        contactsRecView = findViewById(R.id.contactsRecView);
     }
 
 }
