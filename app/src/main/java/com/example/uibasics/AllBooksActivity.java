2aea4ab96779876c44d50aac53bf07ac35aeb1d1
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-09-09 01:52:08 +0100

    Tutorial - "Expandable CardView"

--- app/src/main/java/com/example/uibasics/AllBooksActivity.java
+++ app/src/main/java/com/example/uibasics/AllBooksActivity.java
@@ -1,7 +1,7 @@
 package com.example.uibasics;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,10 +22,11 @@ public class AllBooksActivity extends AppCompatActivity {
         adapter = new BookRecViewAdapter(this);
         booksRecView = findViewById(R.id.booksRecView);
         booksRecView.setAdapter(adapter);
-        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+        booksRecView.setLayoutManager(new LinearLayoutManager(this));
 
         List<Book> books = new ArrayList<>();
         books.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://upload.wikimedia.org/wikipedia/pt/a/a6/1Q84.jpg", "A work of maddening brilliance", "Long Description"));
+        books.add(new Book(2, "The Myth of Sisyphus", "ALbert Camus", 250, "https://upload.wikimedia.org/wikipedia/en/7/75/Le_Mythe_de_Sisyphe.jpg", "One of the most influential works of this century, The Myth of Sisyphus and Other Essays is a crucial exposition of existentialist thought.", "Long Description"));
 
         adapter.setBooks(books);
     }
--- app/src/main/java/com/example/uibasics/Book.java
+++ app/src/main/java/com/example/uibasics/Book.java
@@ -9,6 +9,7 @@ public class Book {
     private String imageUrl;
     private String shortDesc;
     private String longDesc;
+    private boolean isExpanded;
 
     public Book(int id, String name, String author, int pages, String imageUrl, String shortDesc, String longDesc) {
         this.id = id;
@@ -18,6 +19,7 @@ public class Book {
         this.imageUrl = imageUrl;
         this.shortDesc = shortDesc;
         this.longDesc = longDesc;
+        isExpanded = false;
     }
 
     public int getId() {
@@ -74,6 +76,14 @@ public class Book {
 
     public void setLongDesc(String longDesc) {
         this.longDesc = longDesc;
+    }
+
+    public boolean isExpanded() {
+        return isExpanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        isExpanded = expanded;
     }
 
     @Override
