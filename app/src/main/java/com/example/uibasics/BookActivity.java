e2a8985ba0341306105ce7348182610fc900a287
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-09-10 01:30:15 +0100

    Tutorial - "Create Book Activity"

---
+++ app/src/main/java/com/example/uibasics/BookActivity.java
@@ -0,0 +1,61 @@
+package com.example.uibasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class BookActivity extends AppCompatActivity {
+
+    private TextView txtBookName, txtAuthor, txtPages, txtDescription;
+    private Button btnAddToWantToRead, btnAddToAlreadyRead, btnAddToCurrentlyReading, btnAddToFavorite;
+    private ImageView bookImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        initViews();
+
+        String longDescription = "A young woman named Aomame follows a taxi driver’s enigmatic suggestion and begins to notice puzzling discrepancies in the world around her. She has entered, she realizes, a parallel existence, which she calls 1Q84 “Q is for ‘question mark.’ A world that bears a question.” Meanwhile, an aspiring writer named Tengo takes on a suspect ghostwriting project. He becomes so wrapped up with the work and its unusual author that, soon, his previously placid life begins to come unraveled.\n" +
+                "\n" +
+                "As Aomame’s and Tengo’s narratives converge over the course of this single year, we learn of the profound and tangled connections that bind them ever closer: a beautiful, dyslexic teenage girl with a unique vision; a mysterious religious cult that instigated a shoot-out with the metropolitan police; a reclusive, wealthy dowager who runs a shelter for abused women; a hideously ugly private investigator; a mild-mannered yet ruthlessly efficient bodyguard; and a peculiarly insistent television-fee collector.\n" +
+                "\n" +
+                "A love story, a mystery, a fantasy, a novel of self-discovery, a dystopia to rival George Orwell’s 1Q84 is Haruki Murakami’s most ambitious undertaking yet: an instant best seller in his native Japan, and a tremendous feat of imagination from one of our most revered contemporary writers.";
+
+        //TODO: Get the data from recycler view in here
+        Book book = new Book(1, "1Q84", "Haruki Murakami", 1350, "https://upload.wikimedia.org/wikipedia/pt/a/a6/1Q84.jpg", "A work of maddening brilliance", longDescription);
+
+        setData(book);
+    }
+
+    private void setData(Book book) {
+        txtBookName.setText(book.getName());
+        txtAuthor.setText(book.getAuthor());
+        txtPages.setText(String.valueOf(book.getPages()));
+        txtDescription.setText(book.getLongDesc());
+        Glide.with(this)
+                .asBitmap()
+                .load(book.getImageUrl())
+                .into(bookImage);
+    }
+
+    private void initViews() {
+        txtAuthor = findViewById(R.id.txtAuthorName);
+        txtBookName = findViewById(R.id.txtBookName);
+        txtPages = findViewById(R.id.txtPages);
+        txtDescription = findViewById(R.id.txtDescription);
+
+        btnAddToAlreadyRead = findViewById(R.id.btnAddToAlreadyReadList);
+        btnAddToCurrentlyReading = findViewById(R.id.btnAddToCurrentlyReading);
+        btnAddToFavorite = findViewById(R.id.btnAddToFavorite);
+        btnAddToWantToRead = findViewById(R.id.btnAddToWantToReadList);
+
+        bookImage = findViewById(R.id.imgBook);
+    }
+}
