d17734020b55af66679500bab87099d27db0ca69
Raphael Carvalho <ph_rapha5@hotmail.com>
2020-09-12 11:35:56 +0100

    Tutorial - "Animations - Up Button"

--- app/src/main/java/com/example/uibasics/AllBooksActivity.java
+++ app/src/main/java/com/example/uibasics/AllBooksActivity.java
@@ -1,10 +1,12 @@
 package com.example.uibasics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AllBooksActivity extends AppCompatActivity {
 
@@ -16,11 +18,25 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         adapter = new BookRecViewAdapter(this, "allBooks");
         booksRecView = findViewById(R.id.booksRecView);
         booksRecView.setAdapter(adapter);
         booksRecView.setLayoutManager(new LinearLayoutManager(this));
 
         adapter.setBooks(Utils.getInstance().getAllBooks());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
